e695c971ec4b53cf9035678764a9e3e18d80fa6c
milan <acmilanpooh@gmail.com>
2021-02-09 00:26:23 +0900

    DBからデータ呼び出し機能追加

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $data = [
-            'message' => 'これはindex.blade.phpです',
-            'id' => $request->id,
-        ];
-        return view('hello.index', $data);
+        if (isset($request->id))
+        {
+            $params = ['id' => $request->id];
+            $items = DB::select('select * from people where id = :id', $params);
+        } else {
+            $items = DB::select('select * from people');
+        }
+        return view('hello.index', ['items' => $items]);
     }
 }
--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -21,6 +21,17 @@
   @endcomponent
   @include('components.message', ['msg_title' => 'OK', 'msg_content' => 'サブビューです'])
 
+  <table>
+    <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+    @foreach ($items as $item)
+      <tr>
+        <td>{{ $item -> name }}</td>
+        <td>{{ $item -> mail }}</td>
+        <td>{{ $item -> age }}</td>
+      </tr>
+    @endforeach
+  </table>
+
 @endsection
 
 @section('footer')
