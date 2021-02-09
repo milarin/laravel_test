32664ebb5a1d7aa18a7ee08e4735d40eab6320c8
milan <acmilanpooh@gmail.com>
2021-02-09 11:53:29 +0900

    view add

--- laravelapp/app/Http/Controllers/PersonController.php
+++ laravelapp/app/Http/Controllers/PersonController.php
@@ -7,5 +7,9 @@ use App\Models\Person;
 
 class PersonController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $items = Person::all();
+        return view('person.index', ['items' => $items]);
+    }
 }
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -17,3 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('hello', 'HelloController@index');
+Route::get('person', 'PersonController@index');
